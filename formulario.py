a85aef73ad42e8ab0b7902fc38f4e2b9cd96d0ee
AquilesDuarte <aquilesaluno@outlook.com>
2023-12-07 08:26:48 -0300

    second commit

---
+++ formulario.py
@@ -0,0 +1,12 @@
+print('Ola, vamos iniciar esse novo questionario, a principio me diga algumas informacoes suas')
+
+print('\n\n')
+
+nome = str(input('me diga o seu nome completo:'))
+altura = int(input('me diga sua altura em cm:'))
+cpf = int(input('me diga seu cpf:'))
+telefone = int(input('me diga o seu telefone:'))
+
+print('\n\n')
+
+print ('bem, la vamos nos. Seu nome eh ', nome, "sua altura eh ", altura, 'seu cpf eh ', cpf, 'seu telefone eh ', telefone)
